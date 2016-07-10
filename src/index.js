2032f8383fde0af4c3fc2f7c94b63b0c7f5c5d1a
Ken Hibino <ken.hibino7@gmail.com>
2016-07-10 09:02:38 -0700

    fix(export): Fix export modules in index.js file

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
-/*** React Component ***/
 import PlacesAutocomplete from './PlacesAutocomplete'
-export default PlacesAutocomplete
+import { geocodeByAddress } from './utils'
 
-/*** Utils ***/
-export const geocodeByAddress = (address, callback) => {
-  const geocoder = new google.maps.Geocoder()
-  const OK = google.maps.GeocoderStatus.OK
-
-  geocoder.geocode({ address }, (results, status) => {
-    if (status !== OK) {
-      callback({ status }, null)
-      return
-    }
-
-    const latLng = {
-      lat: results[0].geometry.location.lat(),
-      lng: results[0].geometry.location.lng(),
-    }
-
-    callback(null, latLng)
-  })
+export {
+  PlacesAutocomplete,
+  geocodeByAddress
 }
