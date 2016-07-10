8153e6155d05b4553f0e98bcc050fc0b1b2ea455
Ken Hibino <ken.hibino7@gmail.com>
2016-07-09 17:44:12 -0700

    Create package.json & Add a few dependencies

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class PlacesAutocomplete extends React.Component {
+  render() {
+    return (
+      <div>Hello world!</div>
+    );
+  }
+}
+
+export default PlacesAutocomplete;
