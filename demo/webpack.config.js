742651cbca4ff72f91dfd71c1846c2682fa8e97d
cburbank <chris.f.burbank@gmail.com>
2016-12-26 21:53:12 -0500

    feat(demo): Set up demo build config
    Add build settings to webpack config and create demo:build npm script in package.json

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,6 +1,7 @@
 var debug = process.env.NODE_ENV !== 'production'
 var webpack = require('webpack')
 var path = require('path')
+var CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: './demo/index.js',
@@ -21,5 +22,36 @@ module.exports = {
       },
       {test: /\.json$/, loader: 'json'}
     ]
-  }
+  },
+  plugins: process.argv.indexOf('-p') !== -1 ? [
+    new CopyWebpackPlugin([
+      { from: './demo/app.css' },
+      { from: './demo/favicon.ico' },
+      { from: './demo/index.html' }
+    ]),
+    new webpack.DefinePlugin({
+        'process.env':{
+            'NODE_ENV': JSON.stringify('production')
+        }
+    }),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: true,
+      compress: {
+        warnings: false,
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true
+      },
+      output: {
+        comments: false,
+      },
+      exclude: [/\.min\.js$/gi]
+    }),
+    new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
+  ] : []
 }
