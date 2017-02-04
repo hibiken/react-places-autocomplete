382c460615a5b2a430f10f2e6116524a02946c35
Ken Hibino <ken.hibino7@gmail.com>
2017-02-04 12:34:25 -0800

    chore(defaultStyles): Extract default styles to a separate file

--- src/PlacesAutocomplete.js
+++ src/PlacesAutocomplete.js
@@ -1,36 +1,6 @@
 import React from 'react'
+import defaultStyles from './defaultStyles'
 
-const defaultStyles = {
-  root: {
-    position: 'relative',
-    paddingBottom: '0px',
-  },
-  autocompleteOverlay: {
-    position: 'fixed',
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
-    zIndex: 9998,
-  },
-  autocompleteContainer: {
-    position: 'absolute',
-    top: '100%',
-    backgroundColor: 'white',
-    border: '1px solid #555',
-    width: '100%',
-    zIndex: 9999,
-  },
-  autocompleteItem: {
-    backgroundColor: '#ffffff',
-    padding: '10px',
-    color: '#555',
-    cursor: 'pointer',
-  },
-  autocompleteItemActive: {
-    backgroundColor: '#fafafa'
-  }
-}
 
 class PlacesAutocomplete extends React.Component {
   constructor(props) {
