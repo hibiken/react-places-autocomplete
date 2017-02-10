3f06b507297e0b298559b03cd5730b2e9e6a4606
Ken Hibino <ken.hibino7@gmail.com>
2017-02-10 09:44:09 -0800

    fix(onBlur): Remove onBlur handler
    #26

--- src/PlacesAutocomplete.js
+++ src/PlacesAutocomplete.js
@@ -206,7 +206,6 @@ class PlacesAutocomplete extends React.Component {
         value={value}
         onChange={this.handleInputChange}
         onKeyDown={this.handleInputKeyDown}
-        onBlur={() => this.clearAutocomplete()}
         style={styles.input}
         autoFocus={autoFocus}
       />
