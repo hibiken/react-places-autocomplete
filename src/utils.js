c9dbee1c074fae9bb764b0c69578a430ddad24ff
jxom <jakemoxey@gmail.com>
2017-04-25 19:37:20 +1000

    Removed q

--- src/utils.js
+++ src/utils.js
@@ -1,55 +1,51 @@
-import q from 'q'
-
 export const geocodeByAddress = (address, callback) => {
-  const deferred = q.defer()
-  const geocoder = new google.maps.Geocoder()
-  const OK = google.maps.GeocoderStatus.OK
+  return new Promise((resolve, reject) => {
+    const geocoder = new google.maps.Geocoder()
+    const OK = google.maps.GeocoderStatus.OK
 
-  geocoder.geocode({ address }, (results, status) => {
-    if (status !== OK) {
-      if (callback) {
-        callback({ status }, null, results)
+    geocoder.geocode({ address }, (results, status) => {
+      if (status !== OK) {
+        if (callback) {
+          callback({ status }, null, results)
+        }
+        return reject({ error: { status }, results })
       }
-      return deferred.reject({ error: { status }, results })
-    }
 
-    const latLng = {
-      lat: results[0].geometry.location.lat(),
-      lng: results[0].geometry.location.lng(),
-    }
+      const latLng = {
+        lat: results[0].geometry.location.lat(),
+        lng: results[0].geometry.location.lng(),
+      }
 
-    if (callback) {
-      callback(null, latLng, results)
-    }
-    return deferred.resolve({ latLng, results })
+      if (callback) {
+        callback(null, latLng, results)
+      }
+      return resolve({ latLng, results })
+    })
   })
-
-  return deferred.promise
 }
 
 export const geocodeByPlaceId = (placeId, callback) => {
-  const deferred = q.defer()
-  const geocoder = new google.maps.Geocoder()
-  const OK = google.maps.GeocoderStatus.OK
+  return new Promise((resolve, reject) => {
+    const geocoder = new google.maps.Geocoder()
+    const OK = google.maps.GeocoderStatus.OK
 
-  geocoder.geocode({ placeId }, (results, status) => {
-    if (status !== OK) {
-      if (callback) {
-        callback({ status }, null, null)
+    geocoder.geocode({ placeId }, (results, status) => {
+      if (status !== OK) {
+        if (callback) {
+          callback({ status }, null, null)
+        }
+        return reject({ error: { status } })
       }
-      return deferred.reject({ error: { status } })
-    }
 
-    const latLng = {
-      lat: results[0].geometry.location.lat(),
-      lng: results[0].geometry.location.lng(),
-    }
+      const latLng = {
+        lat: results[0].geometry.location.lat(),
+        lng: results[0].geometry.location.lng(),
+      }
 
-    if (callback) {
-      callback(null, latLng, results)
-    }
-    return deferred.resolve({ latLng, results })
+      if (callback) {
+        callback(null, latLng, results)
+      }
+      return resolve({ latLng, results })
+    })
   })
-
-  return deferred.promise
 }
