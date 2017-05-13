1c7f0e8612db46e4e219002f5771a9eaba83946d
Ken Hibino <ken.hibino7@gmail.com>
2017-05-13 13:57:20 -0700

    feat(utilities): Add Promise support to geocoding functions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import PlacesAutocomplete from './PlacesAutocomplete'
-import { geocodeByAddress, geocodeByPlaceId } from './utils'
+import { geocodeByAddress, geocodeByPlaceId, getLatLng } from './utils'
 
 export {
   geocodeByAddress,
   geocodeByPlaceId,
+  getLatLng,
 }
 
 export default PlacesAutocomplete
