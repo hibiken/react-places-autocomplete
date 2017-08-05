32b244406bd569eade7ca03f3a714da0d9694b2c
Ken Hibino <ken.hibino7@gmail.com>
2017-08-05 11:42:49 -0700

    chore(webpack): Use default settings for image-webpack-loader

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
           'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
-          'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+          'image-webpack-loader'
         ]
       }
     ]
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -27,7 +27,7 @@ export default {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
           'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
-          'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+          'image-webpack-loader'
         ],
         include
       }
