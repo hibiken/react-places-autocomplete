70eae87f8f0ac5639c7be8727b0f103ea4b42867
Ken Hibino <ken.hibino7@gmail.com>
2018-01-11 20:36:54 -0800

    style(formatting): Format code with perttier script

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './demo/index.js',
   output: {
     path: __dirname + '/dist',
-    filename: 'index.min.js'
+    filename: 'index.min.js',
   },
   devtool: 'source-map',
   module: {
@@ -17,48 +17,51 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel',
         query: {
-            presets:['es2015', 'react']
-        }
+          presets: ['es2015', 'react'],
+        },
       },
-      {test: /\.json$/, loader: 'json'},
+      { test: /\.json$/, loader: 'json' },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
           'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
-          'image-webpack-loader'
-        ]
-      }
-    ]
+          'image-webpack-loader',
+        ],
+      },
+    ],
   },
-  plugins: process.argv.indexOf('-p') !== -1 ? [
-    new CopyWebpackPlugin([
-      { from: './demo/app.css' },
-      { from: './demo/favicon.ico' },
-      { from: './demo/index.html' }
-    ]),
-    new webpack.DefinePlugin({
-        'process.env':{
-            'NODE_ENV': JSON.stringify('production')
-        }
-    }),
-    new webpack.optimize.AggressiveMergingPlugin(),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      mangle: true,
-      compress: {
-        warnings: false,
-        pure_getters: true,
-        unsafe: true,
-        unsafe_comps: true,
-        screw_ie8: true
-      },
-      output: {
-        comments: false,
-      },
-      exclude: [/\.min\.js$/gi]
-    }),
-    new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
-  ] : []
+  plugins:
+    process.argv.indexOf('-p') !== -1
+      ? [
+          new CopyWebpackPlugin([
+            { from: './demo/app.css' },
+            { from: './demo/favicon.ico' },
+            { from: './demo/index.html' },
+          ]),
+          new webpack.DefinePlugin({
+            'process.env': {
+              NODE_ENV: JSON.stringify('production'),
+            },
+          }),
+          new webpack.optimize.AggressiveMergingPlugin(),
+          new webpack.optimize.DedupePlugin(),
+          new webpack.optimize.OccurrenceOrderPlugin(),
+          new webpack.optimize.UglifyJsPlugin(),
+          new webpack.optimize.UglifyJsPlugin({
+            mangle: true,
+            compress: {
+              warnings: false,
+              pure_getters: true,
+              unsafe: true,
+              unsafe_comps: true,
+              screw_ie8: true,
+            },
+            output: {
+              comments: false,
+            },
+            exclude: [/\.min\.js$/gi],
+          }),
+          new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
+        ]
+      : [],
 }
