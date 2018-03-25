97902dd400a37224e7390e7c0cf0cd6320fd1b22
Ken Hibino <ken.hibino7@gmail.com>
2018-03-24 20:19:56 -0700

    Update tests to use jest

--- src/tests/testHelper.js
+++ src/tests/testHelper.js
@@ -1,25 +1,25 @@
-import chai, { expect } from 'chai';
-import chaiEnzyme from 'chai-enzyme';
-import jsdom from 'jsdom';
-
-/*** Set up test environment to run in a browser-like environment ***/
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
-global.window = global.document.defaultView;
-
-/*** Mock Google Maps JavaScript API ***/
-const google = {
-  maps: {
-    places: {
-      AutocompleteService: () => {},
-      PlacesServiceStatus: {
-        OK: 'OK',
-      },
-    },
-  },
-};
-global.google = google;
-global.window.google = google;
-
-chai.use(chaiEnzyme());
-
-export { expect };
+// import chai, { expect } from 'chai';
+// import chaiEnzyme from 'chai-enzyme';
+// import jsdom from 'jsdom';
+//
+// /*** Set up test environment to run in a browser-like environment ***/
+// global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+// global.window = global.document.defaultView;
+//
+// /*** Mock Google Maps JavaScript API ***/
+// const google = {
+//   maps: {
+//     places: {
+//       AutocompleteService: () => {},
+//       PlacesServiceStatus: {
+//         OK: 'OK',
+//       },
+//     },
+//   },
+// };
+// global.google = google;
+// global.window.google = google;
+//
+// chai.use(chaiEnzyme());
+//
+// export { expect };
