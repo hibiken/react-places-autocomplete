76093778bc3ba50b80cc704e9be5a1f9f61798dd
Ken Hibino <ken.hibino7@gmail.com>
2018-03-30 20:56:28 +0900

    Change travis CI config

--- src/tests/helpers/jestSetup.js
+++ src/tests/helpers/jestSetup.js
@@ -1,4 +1,8 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+// Register babel so that it will transpile ES6 to ES5
+// before our tests run.
+require('babel-register')();
+
+const Enzyme = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
 // React 16 Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() });
