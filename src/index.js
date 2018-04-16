c2d9ca89121aa424adcd18e4f57ee9de76c6b379
Ken Hibino <ken.hibino7@gmail.com>
2018-04-15 19:06:22 -0700

    Updates

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import PlacesAutocomplete from './PlacesAutocomplete';
-import { geocodeByAddress, geocodeByPlaceId, getLatLng } from './utils';
-
-export { geocodeByAddress, geocodeByPlaceId, getLatLng };
-
-export default PlacesAutocomplete;
