63427898daede0820b1e22ab4da26a3864a6e311
Sam Bokai <sam.bokai@gmail.com>
2020-05-17 19:31:25 +0200

    Add null-safe property access in helper function
    Related to #152

--- src/helpers.js
+++ src/helpers.js
@@ -4,7 +4,7 @@ export const compose = (...fns) => (...args) => {
 
 export const pick = (obj, ...props) => {
   return props.reduce((newObj, prop) => {
-    if (obj.hasOwnProperty(prop)) {
+    if (obj && obj.hasOwnProperty(prop)) {
       newObj[prop] = obj[prop];
     }
     return newObj;
